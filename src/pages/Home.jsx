301e1e91351f0745e3c7d3961111d00e3122dd93
Alvaro <alvaro.nrx88@gmail.com>
2024-03-02 22:33:56 -0300

    Novo Portfolio da OXX Digital Solutions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,8 @@ export default function Home() {
             <section className="flex flex-col justify-center items-center space-y-10 w-11/12 max-lg:w-full mx-auto">
 
                 {/* Segundo Cabeçalho */}
-                <div className="oxx-header w-11/12 max-xl:w-full rounded-full bg-[url('luxurious_spaceship.jpg')] bg-center backdrop-blur max-sm:h-[10rem] sm:h-[15rem] xl:h-[20rem] 2xl:h-[30rem] shadow-[inset_0_0_1rem_#000]">
-                    <figure className="bg-[url('products_banner.png')] bg-cover min-h-full min-w-full rounded-full"></figure>
+                <div className="oxx-header w-11/12 max-xl:w-full rounded-full bg-[url('/luxurious_spaceship.jpg')] bg-center backdrop-blur max-sm:h-[10rem] sm:h-[15rem] xl:h-[20rem] 2xl:h-[30rem] shadow-[inset_0_0_1rem_#000]">
+                    <figure className="bg-[url('/products_banner.png')] bg-cover min-h-full min-w-full rounded-full"></figure>
                 </div>
 
                 {/* Conteudo Principal da Pagina */}
